36950a81f5a44c9a132578f5e4cfd4dfc5081e5a
salvoPernicano <salvo.pernicano@gmail.com>
2024-04-04 15:25:54 +0200

    creazione componenti / stampa in console response json / passaggio informazioni a componente figlio tramite props /

---
+++ src/components/AppCards.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+    name :'AppCards',
+    props: [
+    'propsObject'
+    ],
+    data () {
+        return {}
+    }
+}
+</script>
+
+<template>
+    <div class="card">
+        <h1>Nome progetto: {{ propsObject.nome_progetto }}</h1>
+        <p>Descrizione: {{ propsObject.descrizione_progetto }}</p>
+        <span>Linguaggi usati {{ propsObject.linguaggi }}</span>
+    </div>
+</template>
+
+
+<style>
+.card{
+    background-color: lightgray;
+    margin-block: 2em;
+    color: black;
+}
+</style>
---
+++ src/components/MainApp.vue
@@ -0,0 +1,44 @@
+<script>
+import axios from 'axios';
+import AppCards from './AppCards.vue'
+export default{
+  components : {
+    AppCards,
+  },
+  name: 'MainApp',
+  data(){
+    return{
+      projectsArray : [],
+    }
+  },
+  methods: {
+    getData(){
+
+      axios.get('http://127.0.0.1:8000/api/test')
+      .then(response => {
+        console.log(response.data.projects)
+        this.projectsArray = (response.data.projects)
+      })
+    }
+  },
+  mounted() {
+    this.getData()
+  }
+}
+
+</script>
+
+<template>
+ <h1>Lista progetti</h1>
+
+<div class="container">
+  <AppCards v-for="(element,index) in projectsArray" 
+           :key="index"
+           :propsObject="element"/>
+  </div>
+
+</template>
+
+<style >
+
+</style>
