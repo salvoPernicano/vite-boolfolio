7abbc2b5054c7d3c9bcee48c3ef8f7dbfe064cc0
salvoPernicano <salvo.pernicano@gmail.com>
2024-04-11 17:14:25 +0200

    feat base componente form

---
+++ src/pages/ContactForm.vue
@@ -0,0 +1,42 @@
+<script>
+import axios from 'axios'
+import { store } from '../store';
+export default {
+    name : 'ContactForm',
+    data () {
+        return {
+            store,
+            name : '',
+            email : '',
+            message : '',
+            errors : {}
+        }
+    },
+    methods: {
+        sendForm(){
+
+            const data = {
+                name : this.name,
+                email : this.email,
+                message : this.message,
+            };
+
+            this.errors = {};
+
+            axios.post(`${this.store.apiBaseUrl}/api/contacts`, data)
+        }
+    }
+}
+</script>
+
+<template>
+    <div>
+
+    </div>
+</template>
+
+
+
+<style>
+
+</style>
